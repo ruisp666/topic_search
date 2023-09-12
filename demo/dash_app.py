c76ae472102af822e616a044998594e888c47e31
rui sp <manuelsapereira@gmail.com>
2023-09-12 22:38:05 +0800

    Added callback for displaying topic sentiment over time.

--- demo/dash_app.py
+++ demo/dash_app.py
@@ -1,6 +1,8 @@
 """
 This is to show the three topic models and their inttertopic distances
 """
+import json
+
 import requests
 import pandas as pd
 import os
@@ -10,8 +12,8 @@ from bertopic import BERTopic
 import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc
-from dash.dependencies import Input, Output, State
-
+from dash.dependencies import Input, Output
+import plotly.express as px
 from config import SECTIONS_DESCRIPTION, URL_API, SECTIONS_TITLE
 
 
@@ -44,7 +46,12 @@ app.layout = html.Div([
         dcc.Tab(label='Topics over time', children=[
             dcc.Dropdown(id='section-dropdown-time', options=[{'label': s, 'value': s} for s in SECTIONS_TITLE],
                          value='Section1'),
-            dcc.Graph(id='topic-time-plot')])
+            dcc.Graph(id='topic-time-plot')]),
+
+        dcc.Tab(label='Topic sentiment over time', children=[
+            dcc.Dropdown(id='section-dropdown-time-sentiment', options=[{'label': s, 'value': s} for s in SECTIONS_TITLE],
+                       value='Section1'),
+            dcc.Graph(id='topic-time-sentiment-plot')]),
     ])
 ])
 
@@ -76,16 +83,34 @@ def display_intertopic_dist(section):
     Output('topic-time-plot', 'figure'),
     Input('section-dropdown-time', 'value'))
 def display_topic_time(section):
-    data = requests.get(url=os.path.join(URL_API,'get_topics_time'), params={'top_n': '5', 'freq': '1Y'}).json()
-    df = pd.read_json(data['Section1'], orient='records')
+    data = requests.get(url=os.path.join(URL_API,'get_topics_time'), params={'top_n': '5'}).json()
+    df = pd.read_json(data[section], orient='records')
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     df = df.set_index('Timestamp')
-    fig = px.line(data_frame=df, y='Frequency', color='Topic')
+    fig = px.line(data_frame=df, y='Frequency', color='Name')
     fig.update_layout(
     title='Frequency over Time',
     xaxis_title='Snapshot Date',
     yaxis_title='Frequency',
     height=800, width=1200
+    )
+    return fig
+
+@app.callback(
+    Output('topic-time-sentiment-plot', 'figure'),
+    Input('section-dropdown-time-sentiment', 'value'))
+def display_topic_time_sentiment(section):
+    TOP_N = 10
+    data = requests.get(url=os.path.join(URL_API, 'get_topics_sentiment'), params={'freq': '3M'}).json()
+    df = pd.read_json(data[section], orient='records')
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    df = df.set_index('Timestamp')
+    fig = px.line(data_frame=df.loc[df.Topic.isin(range(1, TOP_N)), :], y='mean', color='Name')
+    fig.update_layout(
+        title='Topic Sentiment over time',
+        xaxis_title='Snapshot Date',
+        yaxis_title='Average sentiment',
+        height=800, width=1200
     )
     return fig
 
